3255ca5777a5c75aae7c59839ad82ecd410882f9
xinos <xinosx36@gmail.com>
2023-01-12 12:56:48 +0200

    moved files to src folder

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-from .db_commands import (
-    AddGroupToAllowListCommand,
-    DeleteGroupCommand,
-    DeleteUserCommand,
-    GetGroupCommand,
-    GetUserCommand,
-    AddUserCommand,
-)
-from .db_cleaner import database_cleaner
--- job_posts/__init__.py
+++
@@ -1,1 +0,0 @@
-from .job_post_factory import channel_jobs_updater
